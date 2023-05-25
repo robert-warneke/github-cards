6b1c766e55bb2f238ba32c2ac2d2906a45ef1736
Robert Warneke <hello@robertwarneke.com>
2023-05-24 23:55:43 -0500

    commit

--- api/github-repo-card.js
+++ api/github-repo-card.js
@@ -8,6 +8,7 @@ module.exports = async (req, res) => {
     const user = req.query.user || 'robert-warneke';
     const repo = req.query.repo || 'robert-warneke.github.io';
     const theme = req.query.theme || 'light';
+    const showUsername = req.query.showUsername === 'true' || false;
 
     // Define theme colors based on the selected theme.
     const themeColors = themes[theme];
@@ -19,6 +20,9 @@ module.exports = async (req, res) => {
       },
     });
     const repoData = response.data;
+
+    // Define the title. If showUsername is true, show the username/repo. Otherwise, just show the repo.
+    const title = showUsername ? `${user}/${repo}` : repo;
 
     // Create the dynamic SVG.
     const svg = `
@@ -47,7 +51,7 @@ module.exports = async (req, res) => {
         <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"/>
       </svg>
   
-      <text x="25" y="-2" class="header" data-testid="header">${repo}</text>
+      <text x="25" y="-2" class="header" data-testid="header">${title}</text>
     </g>
   
     <g data-testid="main-card-body" transform="translate(25, 60)">
