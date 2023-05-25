6c5bb55fb312ec0d5e59989f1a79fa5a7ef66531
Robert Warneke <hello@robertwarneke.com>
2023-05-25 00:57:09 -0500

    adding themes

--- api/themes.js
+++ api/themes.js
@@ -1,6 +1,8 @@
 const themes = {
     dark: { background: '#212121', border: '#8ab4f8', title: '#8ab4f8', text: '#f5f5f5', datatext:"#9e9e9e", icon: '#9e9e9e' },
     light: { background: '#f5f5f5', border: '#8ab4f8', title: '#1a0dab', text: '#333333', datatext:"#757575", icon: '#9e9e9e' },
+    plainDark: { background: '#000000', border: '#ffffff', title: '#ffffff', text: '#ffffff', datatext:"#ffffff", icon: '#ffffff' },
+    plainLight: { background: '#ffffff', border: '#000000', title: '#000000', text: '#000000', datatext:"#000000", icon: '#000000' },
     // ...any other themes you want to define
   };
   
--- docs/THEMES.md
+++ docs/THEMES.md
@@ -21,3 +21,29 @@ dark
 GitHub Repo Card:
 
 ![GitHub Repo Card](https://my-github-cards.vercel.app/api/github-repo-card?user=robert-warneke&repo=github-cards&theme=dark)
+
+---
+
+## `plainLight`
+
+```
+plainLight
+```
+
+GitHub Repo Card:
+
+![GitHub Repo Card](https://my-github-cards.vercel.app/api/github-repo-card?user=robert-warneke&repo=github-cards&theme=plainLight)
+
+---
+
+## `plainDark`
+
+```
+plainDark
+```
+
+GitHub Repo Card:
+
+![GitHub Repo Card](https://my-github-cards.vercel.app/api/github-repo-card?user=robert-warneke&repo=github-cards&theme=plainDark)
+
+---
