4d59a3f23a04a68d2eca7adc87091d80bf8e71f1
Robert Warneke <hello@robertwarneke.com>
2023-05-24 23:12:43 -0500

    commit

--- api/themes.js
+++ api/themes.js
@@ -1,6 +1,6 @@
 const themes = {
     dark: { background: '#333333', border: '#8ab4f8', title: '#8ab4f8', text: '#bdbdbd', datatext:"#9e9e9e", icon: '#9e9e9e' },
-    light: { background: '#f5f5f5', border: '#a6a6a6', title: '#1a0dab', text: '#616161', datatext:"#757575", icon: '#9e9e9e' },
+    light: { background: '#f5f5f5', border: '#1a0dab', title: '#1a0dab', text: '#616161', datatext:"#757575", icon: '#9e9e9e' },
     // ...any other themes you want to define
   };
   
