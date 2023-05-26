9660e14fc86e496eb9537cd36d7e844f017efc73
Robert Warneke <hello@robertwarneke.com>
2023-05-26 10:41:18 -0500

    themes

--- api/themes.js
+++ api/themes.js
@@ -1,6 +1,6 @@
 const themes = {
-    dark: { background: '#212121', border: '#8ab4f8', title: '#8ab4f8', description: '#f5f5f5', dataText:"#9e9e9e", icon: '#9e9e9e' },
-    light: { background: '#f5f5f5', border: '#8ab4f8', title: '#1a0dab', description: '#333333', dataText:"#757575", icon: '#9e9e9e' },
+    dark: { background: '#212121', border: '#8ab4f8', title: '#8ab4f8', description: '#f5f5f5', dataText:"#8ab4f8", icon: '#9e9e9e' },
+    light: { background: '#f5f5f5', border: '#8ab4f8', title: '#1a0dab', description: '#333333', dataText:"#1a0dab", icon: '#9e9e9e' },
     straightBlack: { background: '#000000', border: '#ffffff', title: '#ffffff', description: '#ffffff', dataText:"#ffffff", icon: '#ffffff' },
     straightWhite: { background: '#ffffff', border: '#000000', title: '#000000', description: '#000000', dataText:"#000000", icon: '#000000' },
     // ...any other themes you want to define
