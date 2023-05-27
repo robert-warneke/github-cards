52f2d8fbf1233f4b9a4dda8cb3286b31445a024a
Robert Warneke <hello@robertwarneke.com>
2023-05-27 13:26:03 -0500

    commit

--- api/github-repo-card.js
+++ api/github-repo-card.js
@@ -26,6 +26,10 @@ module.exports = async (req, res) => {
     });
 
     const repoData = response.data;
+    const repoStarData = repoData.stargazers_count;
+    const repoWatchData = repoData.subscribers_count;
+    const repoForkData = repoData.forks_count;
+    const repoDescriptionData = repoData.description;
 
     const lang = repoData.language;
     const langData = languages[lang];
@@ -114,7 +118,7 @@ module.exports = async (req, res) => {
       <foreignObject x="0" y="-20" width="350" height="50">
         <div xmlns="http://www.w3.org/1999/xhtml" class="description">
           <span class="truncate">
-            ${repoData.description}
+            ${repoDescriptionData}
           </span>
         </div>
       </foreignObject>
@@ -137,19 +141,19 @@ module.exports = async (req, res) => {
         <svg class="icon" x="180" y="0" viewBox="0 0 16 16" version="1.1" width="16" height="16">
           <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"/>
         </svg>
-        <text data-testid="stargazers" class="datatext" x="200" y="12">${repoData.stargazers_count}</text>
+        <text data-testid="stargazers" class="datatext" x="200" y="12">${repoStarData}</text>
   
         <!-- Watching icon -->
         <svg class="icon" x="240" y="0" viewBox="0 0 16 16" version="1.1" width="16" height="16">
           <path d="M8 3C4.2 3 1 6 1 8s3.2 5 7 5 7-2 7-5-3.2-5-7-5zm0 8c-1.7 0-3-1.3-3-3s1.3-3 3-3 3 1.3 3 3-1.3 3-3 3zm0-5c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
         </svg>
-        <text data-testid="watching" class="datatext" x="260" y="12">${repoData.subscribers_count}</text>
+        <text data-testid="watching" class="datatext" x="260" y="12">${repoWatchData}</text>
   
         <!-- Forks icon -->
         <svg class="icon" x="300" y="0" viewBox="0 0 16 16" version="1.1" width="16" height="16">
           <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
         </svg>
-        <text data-testid="forkcount" class="datatext" x="320" y="12">${repoData.forks_count}</text>
+        <text data-testid="forkcount" class="datatext" x="320" y="12">${repoForkData}</text>
       </g>
     </g>
     </svg>
